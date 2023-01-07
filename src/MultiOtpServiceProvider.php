ae90eb09eeb95a64aa139373c6b407dc033b2d78
Iman Borumand Zadeh <imanborumand@gmail.com>
2023-01-07 07:01:55 +0000

    initial commit

---
+++ src/MultiOtpServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Imanborumand\MultiOtp;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiOtpServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
